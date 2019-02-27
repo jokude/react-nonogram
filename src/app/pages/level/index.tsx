697d2e955234484db2d081b21ac9587525962df2
Richard Hidalgo <hidalgo931@gmail.com>
2019-02-26 23:37:46 -0300

    apply highlight context provider

--- src/app/pages/level/index.tsx
+++ src/app/pages/level/index.tsx
@@ -4,6 +4,7 @@ import { ILevel } from "Types/Level";
 import { getCategory, transformName } from "../../datasource/datasource";
 import { Topbar } from "../core/Topbar";
 import { GameStateProvider } from "./components/GameContext";
+import { HighlightStateProvider } from "./components/GameContext/highlight";
 import { Grid } from "./components/Grid";
 import { HorizontalHints, VerticalHints } from "./components/Hints";
 import { Timer } from "./components/Timer";
@@ -16,18 +17,20 @@ export const Level: React.FunctionComponent<RouteChildrenProps<{ categoryId: str
   const categoryLevel = category.levels.find((level: ILevel) => transformName(level.title) === levelId);
   return (
     <GameStateProvider size={category.size} level={categoryLevel.grid}>
-      <div>
-        <Topbar>
-          <Timer countdownSeconds={category.countdownMinutes * 60} />
-        </Topbar>
-        <div style={{ userSelect: "none" }} onContextMenu={onContextMenuHandler}>
-          <VerticalHints size={category.size} level={categoryLevel.grid} />
-          <div style={{ display: "flex" }}>
-            <HorizontalHints size={category.size} level={categoryLevel.grid} />
-            <Grid size={category.size} />
+      <HighlightStateProvider>
+        <div>
+          <Topbar>
+            <Timer countdownSeconds={category.countdownMinutes * 60} />
+          </Topbar>
+          <div style={{ userSelect: "none" }} onContextMenu={onContextMenuHandler}>
+            <VerticalHints size={category.size} level={categoryLevel.grid} />
+            <div style={{ display: "flex" }}>
+              <HorizontalHints size={category.size} level={categoryLevel.grid} />
+              <Grid size={category.size} />
+            </div>
           </div>
         </div>
-      </div>
+      </HighlightStateProvider>
     </GameStateProvider>
   );
 };
