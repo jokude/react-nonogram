dccdbde04dbcdec16c7338dfea23651bf45a2891
Richard Hidalgo <hidalgo931@gmail.com>
2019-07-16 18:18:47 -0400

    set proper component prop types

--- src/app/commons/components/Icon.tsx
+++ src/app/commons/components/Icon.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 const baseUrl = `${BASE_PATH}assets/svg/`;
 
-export const Icon: React.FunctionComponent<React.ImgHTMLAttributes<any>> = ({ src, ...props }) => (
+export const Icon: React.FunctionComponent<React.ImgHTMLAttributes<HTMLImageElement>> = ({ src, ...props }) => (
   <img src={`${baseUrl}${src}.svg`} {...props} />
 );
