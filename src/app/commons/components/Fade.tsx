547ef84cee554a125a9815340cbbae1e55dd17e3
Richard Hidalgo <hidalgo931@gmail.com>
2019-07-16 18:17:29 -0400

    replace transition by css transition

--- src/app/commons/components/Fade.tsx
+++ src/app/commons/components/Fade.tsx
@@ -1,33 +1,41 @@
 import * as React from "react";
-import { Transition } from "react-transition-group";
-import { TransitionProps, TransitionStatus } from "react-transition-group/Transition";
+import { CSSTransition } from "react-transition-group";
+import { TransitionProps } from "react-transition-group/Transition";
+import styled from "styled-components";
 
-const duration = 300;
+const DURATION_MILLISECONDS = 300;
+const FADE_CLASSNAME = "fade";
 
-const defaultStyle: React.CSSProperties = {
-  position: "absolute",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0,
-};
+const Container = styled.div`
+  bottom: 0;
+  left: 0;
+  opacity: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  transition: opacity ${DURATION_MILLISECONDS}ms ease-in-out;
 
-const transitionStyles: Record<TransitionStatus, React.CSSProperties> = {
-  entering: { opacity: 0 },
-  entered:  { opacity: 1 },
-  exiting:  { opacity: 0 },
-  exited:  { opacity: 0 },
-  unmounted:  { opacity: 0 },
-};
+  &.${FADE_CLASSNAME}-enter,
+   .${FADE_CLASSNAME}-enter-active,
+   .${FADE_CLASSNAME}-exit-active,
+   .${FADE_CLASSNAME}-exit-done {
+    opacity: 0;
+  }
+  &.${FADE_CLASSNAME}-enter-done,
+   .${FADE_CLASSNAME}-exit {
+    opacity: 1;
+  }
+  &.${FADE_CLASSNAME}-exit,
+  .${FADE_CLASSNAME}-exit-active,
+  .${FADE_CLASSNAME}-exit-done {
+   opacity: 0;
+ }
+`;
 
-export const Fade: React.FunctionComponent<Partial<TransitionProps>> = ({ children, ...props }) => (
-  <Transition timeout={{enter: 300, exit: 300}} {...props}>
-    {(state) => (
-      <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
-        {children}
-      </div>
-    )}
-  </Transition>
+export const FadeTransition: React.FunctionComponent<Partial<TransitionProps>> = ({ children, ...props }) => (
+  <CSSTransition timeout={DURATION_MILLISECONDS} classNames={FADE_CLASSNAME} {...props}>
+    <Container>
+      {children}
+    </Container>
+  </CSSTransition>
 );
