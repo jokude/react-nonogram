3dba8a1269b2f14974750c900392959aaa34ca7b
Richard Hidalgo <hidalgo931@gmail.com>
2019-07-16 18:21:56 -0400

    use absolute position only on big screens

--- src/app/commons/components/MainLayout.tsx
+++ src/app/commons/components/MainLayout.tsx
@@ -8,16 +8,16 @@ export const MainLayout = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
 
-  @media (min-width: ${Breakpoints.lg}px) {
+  @media (min-width: ${Breakpoints.lg + 40}px) {
     border: 10px solid #888098;
     border-radius: 10px;
     max-width: ${Breakpoints.lg}px;
     max-height: 800px;
     position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
   }
 `;
