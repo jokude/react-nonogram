c21235b4c4e552efe43bb016d8aae32e3f89e3f7
Richard Hidalgo <hidalgo931@gmail.com>
2019-07-20 22:38:43 -0400

    update category and level types

--- src/app/types/Category.ts
+++ src/app/types/Category.ts
@@ -1,10 +1,7 @@
-import { ILevel } from "./Level";
-
 export type CategorySize = 5 | 10 | 15;
 
 export interface ICategory {
   title: string;
   size: CategorySize;
-  levels: ILevel[];
   countdownMinutes: number;
 }
--- src/app/types/Level.ts
+++ src/app/types/Level.ts
@@ -1,7 +1,13 @@
 import { ITimer } from "Commons/timer/types";
+import { ICategory } from "Types/Category";
 
 export interface ILevel {
+  category: ICategory;
   title: string;
   grid: boolean[][];
+}
+
+export interface ILevelSummary {
+  title: string;
   timeResult: ITimer | undefined;
 }
